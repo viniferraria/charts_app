ca74599450031b36dd9d818dd662c1736a44a9fe
viniciusferraria <vinicius.ferraria@conductor.com.br>
2019-09-04 16:41:32 -0300

    initializing repository

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+
+import 'dart:math';
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+import 'bar.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      scaffoldBackgroundColor: Colors.greenAccent,
+      
+    ),
+    home: ChartPage()));
+}
+
+class ChartPage extends StatefulWidget {
+  @override
+  ChartPageState createState() => ChartPageState();
+}
+
+class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
+  final random = Random();
+  AnimationController animation;
+  BarTween tween;
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    tween = BarTween(Bar(0.0), Bar(50.0));
+    animation.forward();
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  void changeData() {
+    setState(() {
+      tween = BarTween(
+        tween.evaluate(animation),
+        Bar(random.nextDouble() * 100),
+      );
+      animation.forward(from: 0.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CustomPaint(
+          size: Size(200.0, 100.0),
+          painter: BarChartPainter(tween.animate(animation)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: changeData,
+      ),
+    );
+  }
+}
